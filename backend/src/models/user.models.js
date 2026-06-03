d27c2129f4fdb9c412894236bd2093058d5447bf
megha-56 <meghapanthi435@gmail.com>
2026-06-03 19:12:58 +0530

    ..

--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -1,45 +1,45 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const userSchema=new mongoose.Schema({
-    name: {
-        type:String,
-        required:true
-    },
-    username: {
-        type:String,
-        required:true,
-        unique:true
-    },
-    email: {
-        type:String,
-        required:true,
-        unique:true
-    }, 
-    phoneNo: {
-        type:String,
-        required:true,
-        unique:true
-    }, 
-    password:{
-        type:String,
-        required:true
-    } ,
-    pfp:{
-        type:String
-    }, 
-    gender:{
-        type:String
-    },
-    dob:{
-        type:Date
-    },
-    bio:{
-        type:String
-    },
-    skills:{
-        type: [String]
-    }
-    
-},{timestamps:true});
+// const userSchema=new mongoose.Schema({
+//     name: {
+//         type:String,
+//         required:true
+//     },
+//     username: {
+//         type:String,
+//         required:true,
+//         unique:true
+//     },
+//     email: {
+//         type:String,
+//         required:true,
+//         unique:true
+//     }, 
+//     phoneNo: {
+//         type:String,
+//         required:true,
+//         unique:true
+//     }, 
+//     password:{
+//         type:String,
+//         required:true
+//     } ,
+//     pfp:{
+//         type:String
+//     }, 
+//     gender:{
+//         type:String
+//     },
+//     dob:{
+//         type:Date
+//     },
+//     bio:{
+//         type:String
+//     },
+//     skills:{
+//         type: [String]
+//     }
 
-export const User=mongoose.model("User",userSchema);
+// },{timestamps:true});
+
+// export const User=mongoose.model("User",userSchema);
