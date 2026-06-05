0b3cada161cc4036e996e441512e2008515f5357
megha-56 <meghapanthi435@gmail.com>
2026-06-05 23:48:30 +0530

    Made route for editProfile controller

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { loginUser, registerUser } from "../controllers/user.controllers.js";
+import { editProfile, loginUser, registerUser } from "../controllers/user.controllers.js";
 
 const router=Router();
 
 router.post("/register",registerUser);
 router.post("/login",loginUser);
+router.post("/editprofile",editProfile)
 
 
 
